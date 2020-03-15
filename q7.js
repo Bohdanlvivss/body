140b7d47badf82b4dc92a3ae95e5c3edc683ad42
Bohdanlvivss <62074618+Bohdanlvivss@users.noreply.github.com>
2020-03-15 13:15:55 +0200

    Delete q7.js

--- q7.js
+++
@@ -1,5 +0,0 @@
-let login = prompt("login?");
-let email = prompt("mail");
-let pasword = prompt("pass");
-alert("your login: "+ login + "your email: "+email +"your password: "+pasword);
-document.write("your login: "+ login + "your email: "+email +"your password: "+pasword);
